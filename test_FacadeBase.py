0ccbb204e0cbad7b1019538cf57144f353dec8fe
eladgunders <gunderselad@gmail.com>
2022-02-18 15:03:33 +0200

    FacadeBase tests Done

--- test_FacadeBase.py
+++ test_FacadeBase.py
@@ -6,6 +6,7 @@ from Airline_Company import Airline_Company
 from datetime import datetime
 from User import User
 from DbRepoPool import DbRepoPool
+from NotValidDataError import NotValidDataError
 
 
 @pytest.fixture(scope='session')
@@ -42,10 +43,7 @@ def test_facade_base_get_flight_by_id(dao_connection_singleton, id_, expected):
     assert actual == [expected]
 
 
-@pytest.mark.parametrize('ocountry_id, dcountry_id, date_, expected', [('f', 3, datetime(2022, 1, 30), None),
-                                                                       (3, 'r', datetime(2022, 1, 30), None),
-                                                                       (0, 4, datetime(2022, 1, 30), None),
-                                                                       (1, 2, 4, None),
+@pytest.mark.parametrize('ocountry_id, dcountry_id, date_, expected', [
                                                                        (1, 2, datetime(2022, 1, 30), [Flight(id=1, airline_company_id=1, origin_country_id=1, destination_country_id=2,
                         departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0),
                              remaining_tickets=200), Flight(id=2, airline_company_id=2, origin_country_id=1, destination_country_id=2,
@@ -57,14 +55,22 @@ def test_facade_base_get_flights_by_parameters(dao_connection_singleton, ocountr
     assert actual == expected
 
 
+@pytest.mark.parametrize('ocountry_id, dcountry_id, date_', [('f', 3, datetime(2022, 1, 30)),
+                                                             (3, 'r', datetime(2022, 1, 30)),
+                                                             (0, 4, datetime(2022, 1, 30)),
+                                                             (1, 2, 4)])
+def test_facade_base_get_flights_by_parameters_raise_notvaliddataerror(dao_connection_singleton, ocountry_id, dcountry_id, date_):
+    with pytest.raises(NotValidDataError):
+        dao_connection_singleton.get_flights_by_parameters(ocountry_id, dcountry_id, date_)
+
+
 def test_facade_base_get_all_airlines(dao_connection_singleton):
     actual = dao_connection_singleton.get_all_airlines()
     assert actual == [Airline_Company(id=1, name='Yoni', country_id=1, user_id=3),
                       Airline_Company(id=2, name='Yishay', country_id=2, user_id=4)]
 
 
-@pytest.mark.parametrize('id_, expected', [('h', None),
-                                           (0, None),
+@pytest.mark.parametrize('id_, expected', [
                                            (3, []),
                                            (1, [Airline_Company(id=1, name='Yoni', country_id=1, user_id=3)]),
                                            (2, [Airline_Company(id=2, name='Yishay', country_id=2, user_id=4)])])
@@ -73,13 +79,18 @@ def test_facade_base_get_airline_by_id(dao_connection_singleton, id_, expected):
     assert actual == expected
 
 
+@pytest.mark.parametrize('country_id', [('h'), (0)])
+def test_facade_base_get_airline_by_id_raise_notvaliddataerror(dao_connection_singleton, country_id):
+    with pytest.raises(NotValidDataError):
+        dao_connection_singleton.get_airline_by_id(country_id)
+
+
 def test_facade_base_get_all_countries(dao_connection_singleton):
     actual = dao_connection_singleton.get_all_countries()
     assert actual == [Country(id=1, name='Israel'), Country(id=2, name='Germany')]
 
 
-@pytest.mark.parametrize('id_, expected', [('6', None),
-                                           (0, None),
+@pytest.mark.parametrize('id_, expected', [
                                            (3, []),
                                            (1, [Country(id=1, name='Israel')]),
                                            (2, [Country(id=2, name='Germany')])])
@@ -88,21 +99,37 @@ def test_facade_base_get_country_by_id(dao_connection_singleton, id_, expected):
     assert actual == expected
 
 
-@pytest.mark.parametrize('user, expected', [('notuser', None),
-                                            (User(username='Elad', password='123', email='eladi@gmail.com', user_role=1), None),
-                                            (User(username='Elados', password='123', email='elad@gmail.com', user_role=2), None),
-                                            (User(username='Elados', password='123', email='eladi@gmail.com', user_role=5), None),
+@pytest.mark.parametrize('country_id', [('6'), (0)])
+def test_facade_base_get_country_by_id_raise_notvaliddataerror(dao_connection_singleton, country_id):
+    with pytest.raises(NotValidDataError):
+        dao_connection_singleton.get_country_by_id(country_id)
+
+
+@pytest.mark.parametrize('user, expected', [
                                             (User(username='Elados', password='123', email='eladi@gmail.coom', user_role=2), True)])
 def test_facade_base_create_user(dao_connection_singleton, user, expected):
     actual = dao_connection_singleton.create_user(user)
     assert actual == expected
 
 
-@pytest.mark.parametrize('airline_id, expected', [('not int', None),
-                                                  (0, None),
-                                                  (7, None),
+@pytest.mark.parametrize('user', [('notuser'),
+                                  (User(username='Elad', password='123', email='eladi@gmail.com', user_role=1)),
+                                  User(username='Elados', password='123', email='elad@gmail.com', user_role=2),
+                                  User(username='Elados', password='123', email='eladi@gmail.com', user_role=5)])
+def test_facade_base_create_user_raise_notvaliddataerror(dao_connection_singleton, user):
+    with pytest.raises(NotValidDataError):
+        dao_connection_singleton.create_user(user)
+
+
+@pytest.mark.parametrize('airline_id, expected', [
                                                   (1, [Flight(id=1, airline_company_id=1, origin_country_id=1, destination_country_id=2,
                         departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200)])])
-def test_airline_facade_get_flights_by_airline(dao_connection_singleton, airline_id, expected):
+def test_airline_facade_get_flights_by_airline_id(dao_connection_singleton, airline_id, expected):
     actual = dao_connection_singleton.get_flights_by_airline_id(airline_id)
     assert actual == expected
+
+
+@pytest.mark.parametrize('airline_id', ['not int', 0, 7])
+def test_facade_base_get_flights_by_airline_id_raise_notvaliddataerror(dao_connection_singleton, airline_id):
+    with pytest.raises(NotValidDataError):
+        dao_connection_singleton.get_flights_by_airline_id(airline_id)
