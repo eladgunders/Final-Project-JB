a6b16f76d9cc9c66cd3f378ba97736ab4e19e57c
eladgunders <gunderselad@gmail.com>
2022-02-19 19:01:32 +0200

    AdministratorFacade tests Done

--- test_AdministratorFacade.py
+++ test_AdministratorFacade.py
@@ -4,6 +4,7 @@ from User import User
 from Administrator import Administrator
 from Customer import Customer
 from Airline_Company import Airline_Company
+from NotValidDataError import NotValidDataError
 from DbRepoPool import DbRepoPool
 
 
@@ -13,7 +14,7 @@ def administrator_facade_object():
     repool = DbRepoPool.get_instance()
     repo = repool.get_connection()
     anonfacade = AnonymousFacade(repo)
-    return anonfacade.login('Tomer', '123')
+    return anonfacade.login('Boris', '123')
 
 
 @pytest.fixture(autouse=True)
@@ -22,15 +23,20 @@ def reset_db(administrator_facade_object):
     return
 
 
-@pytest.mark.parametrize('user, administrator, expected', [('not user', 2, None),
-                                                           (User(username='Elad', password='123', email='elad@gmail.com', user_role=9), 2, None),
-                                                           (User(username='Elad', password='123', email='elad@gmail.com', user_role=3), 2, None),
-                                                           (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=3), 'k', None),
-                                                           (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=3),
+@pytest.mark.parametrize('user, administrator, expected', [(User(username='Eladi', password='123', email='eladi@gmail.com', user_role=3),
                                                             Administrator(first_name='Borissss', last_name='Boriiii', user_id=6), True)])
 def test_administrator_facade_add_administrator(administrator_facade_object, user, administrator, expected):
     actual = administrator_facade_object.add_administrator(user, administrator)
     assert actual == expected
+
+
+@pytest.mark.parametrize('user, administrator', [('not user', 2),
+                                                 (User(username='Elad', password='123', email='elad@gmail.com', user_role=9), 2),
+                                                 (User(username='Elad', password='123', email='elad@gmail.com', user_role=3), 2),
+                                                 (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=3), 'k')])
+def test_administrator_facade_add_administrator_raise_notvaliddataerror(administrator_facade_object, user, administrator):
+    with pytest.raises(NotValidDataError):
+        administrator_facade_object.add_administrator(user, administrator)
 
 
 def test_administrator_facade_get_all_customers(administrator_facade_object):
@@ -42,51 +48,43 @@ def test_administrator_facade_get_all_customers(administrator_facade_object):
     assert actual == expected
 
 
-@pytest.mark.parametrize('admin_id, expected', [('not int', None),
-                                                (-1, None),
-                                                (3, None),
-                                                (1, True)])
+@pytest.mark.parametrize('admin_id, expected', [(1, True)])
 def test_administrator_facade_remove_administrator(administrator_facade_object, admin_id, expected):
     actual = administrator_facade_object.remove_administrator(admin_id)
     assert actual == expected
 
 
-@pytest.mark.parametrize('customer_id, expected', [('f', None),
-                                                   (0, None),
-                                                   (3, None),
-                                                   (1, True),
-                                                   (2, True)])
+@pytest.mark.parametrize('admin_id', ['not int', -1, 3])
+def test_administrator_facade_remove_administrator_raise_notvaliddataerror(administrator_facade_object, admin_id):
+    with pytest.raises(NotValidDataError):
+        administrator_facade_object.remove_administrator(admin_id)
+
+
+@pytest.mark.parametrize('customer_id, expected', [(1, True), (2, True)])
 def test_administrator_facade_remove_customer(administrator_facade_object, customer_id, expected):
     actual = administrator_facade_object.remove_customer(customer_id)
     assert actual == expected
 
 
-@pytest.mark.parametrize('airline_id, expected', [('f', None),
-                                                  (-1, None),
-                                                  (4, None),
-                                                  (1, True)])
+@pytest.mark.parametrize('customer_id', ['f', 0, 3])
+def test_administrator_facade_remove_customer_raise_notvaliddataerror(administrator_facade_object, customer_id):
+    with pytest.raises(NotValidDataError):
+        administrator_facade_object.remove_customer(customer_id)
+
+
+@pytest.mark.parametrize('airline_id, expected', [(1, True)])
 def test_administrator_facade_remove_airline(administrator_facade_object, airline_id, expected):
     actual = administrator_facade_object.remove_airline(airline_id)
     assert actual == expected
 
 
-@pytest.mark.parametrize('user, customer, expected', [(1, 1, None), (User(username='Elad', password='123', email='eladi@gmail.com', user_role=2),
-                                                       Customer(first_name='kk', last_name='lk', address='Sokolov 1',
-                                                        phone_no='0545557000', credit_card_no='0099', user_id=1) , None),
-                                                      (User(username='Elados', password='123', email='eladi@gmail.coom', user_role=2),
-                                                       Customer(first_name='kk', last_name='lk', address='Sokolov 1',
-                                                                phone_no='0545557000', credit_card_no='0099', user_id=1), None),
-                                                      (User(username='Elad', password='123', email='eladi@gmail.com', user_role=1),
-                                                       Customer(first_name='kk', last_name='lk', address='Sokolov 1',
-                                                        phone_no='0545557000', credit_card_no='0099', user_id=1) , None),
-                                                      (User(username='Elados', password='123', email='eladi@gmail.coom', user_role=2),
-                                                       'g', None),
-                                                      (User(username='Elados', password='123', email='eladii@gmail.com', user_role=1),
-                                                       Customer(first_name='kk', last_name='lk', address='Sokolov 1',
-                                                        phone_no='0545557007', credit_card_no='0099', user_id=1) , None),
-                                                      (User(username='Elados', password='123', email='eladii@gmail.com', user_role=1),
-                                                       Customer(first_name='kk', last_name='lk', address='Sokolov 1',
-                                                        phone_no='0545557004', credit_card_no='0000', user_id=1) , None),
+@pytest.mark.parametrize('airline_id', ['f', -1, 4])
+def test_administrator_facade_remove_airline_raise_notvaliddataerror(administrator_facade_object, airline_id):
+    with pytest.raises(NotValidDataError):
+        administrator_facade_object.remove_airline(airline_id)
+
+
+@pytest.mark.parametrize('user, customer, expected', [
                                                       (User(username='Elados', password='123', email='eladii@gmail.com', user_role=1),
                                                        Customer(first_name='kk', last_name='lk', address='Sokolov 1',
                                                         phone_no='0545557004', credit_card_no='0055', user_id=8) , True)
@@ -96,16 +94,51 @@ def test_administrator_facade_add_customer(administrator_facade_object, user, cu
     assert actual == expected
 
 
-@pytest.mark.parametrize('user, airline, expected', [(1, 1, None),
-                                                     (User(username='Elad', password='123', email='eladi@gmail.com', user_role=1), 1, None),
-                                                     (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=2), 1, None),
-                                                     (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=2),
-                                                      Airline_Company(name='Yoni', country_id=1, user_id=3), None),
-                                                     (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=2),
-                                                      Airline_Company(name='Yonchkin', country_id=3, user_id=3), None),
+@pytest.mark.parametrize('user, customer', [(1, 1),
+                                            (
+                                            User(username='Elad', password='123', email='eladi@gmail.com', user_role=2),
+                                            Customer(first_name='kk', last_name='lk', address='Sokolov 1',
+                                                     phone_no='0545557000', credit_card_no='0099', user_id=1)),
+                                            (User(username='Elados', password='123', email='eladi@gmail.coom',
+                                                  user_role=2),
+                                             Customer(first_name='kk', last_name='lk', address='Sokolov 1',
+                                                      phone_no='0545557000', credit_card_no='0099', user_id=1)),
+                                                (User(username='Elad', password='123', email='eladi@gmail.com',
+                                                      user_role=1),
+                                                 Customer(first_name='kk', last_name='lk', address='Sokolov 1',
+                                                          phone_no='0545557000', credit_card_no='0099', user_id=1)),
+                                                (User(username='Elados', password='123', email='eladi@gmail.coom',
+                                                      user_role=2), 'g'),
+                                            (User(username='Elados', password='123', email='eladii@gmail.com',
+                                                 user_role=1),
+                                            Customer(first_name='kk', last_name='lk', address='Sokolov 1',
+                                                     phone_no='0545557007', credit_card_no='0099', user_id=1)),
+                                            (User(username='Elados', password='123', email='eladii@gmail.com',
+                                                  user_role=1),
+                                             Customer(first_name='kk', last_name='lk', address='Sokolov 1',
+                                                      phone_no='0545557004', credit_card_no='0000', user_id=1))])
+def test_administrator_facade_add_customer_raise_notvaliddataerror(administrator_facade_object, user, customer):
+    with pytest.raises(NotValidDataError):
+        administrator_facade_object.add_customer(user, customer)
+
+
+@pytest.mark.parametrize('user, airline, expected', [
                                                      (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=2),
                                                       Airline_Company(name='Yonchkin', country_id=1, user_id=8), True)])
 def test_administrator_facade_add_airline(administrator_facade_object, user, airline, expected):
     actual = administrator_facade_object.add_airline(user, airline)
     assert actual == expected
 
+
+@pytest.mark.parametrize('user, airline', [(1, 1),
+                                           (User(username='Elad', password='123', email='eladi@gmail.com', user_role=1), 1),
+                                           (User(username='Eladi', password='123', email='eladi@gmail.com', user_role=2), 1),
+                                           (
+                                           User(username='Eladi', password='123', email='eladi@gmail.com', user_role=2),
+                                           Airline_Company(name='Yoni', country_id=1, user_id=3)),
+                                           (
+                                           User(username='Eladi', password='123', email='eladi@gmail.com', user_role=2),
+                                           Airline_Company(name='Yonchkin', country_id=3, user_id=3))])
+def test_administrator_facade_add_airline_raise_notvaliddataerror(administrator_facade_object, user, airline):
+    with pytest.raises(NotValidDataError):
+        administrator_facade_object.add_customer(user, airline)
