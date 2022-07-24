b8b6d6a73f6cad93970c3769b3d18651b5fe1a5a
Elad Gunders <gunderselad@gmail.com>
2022-07-24 09:03:35 +0300

    customer blueprint done

--- main_server.py
+++ main_server.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, session, jsonify
 from flask_cors import CORS
 from airline.airline import airline
 from admin.admin import admin
+from customer.customer import customer
 
 
 app = Flask(__name__)
 
+app.register_blueprint(customer, url_prefix="/customer")
 app.register_blueprint(airline, url_prefix="/airline")
 app.register_blueprint(admin, url_prefix="/admin")
+
 app.config['SECRET_KEY']: str = 'SHHH KEEP IT SECRET'  # for the jwt encoding
 
 CORS(app)
